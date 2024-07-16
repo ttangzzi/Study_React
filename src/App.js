8f0423bd2d19918e37a822e14bbdd709f647bf58
ttangzzi <cvbg0802@gmail.com>
2024-07-16 23:26:11 +0900

    react useState 사용

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let [title, b] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬 독학']);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>블로그</h4>
+      </div>
+      <div className="list">
+        <h4>{title[0]}</h4>
+        <p>7월 16일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{title[1]}</h4>
+        <p>7월 16일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{title[2]}</h4>
+        <p>7월 16일 발행</p>
+      </div>
     </div>
   );
 }

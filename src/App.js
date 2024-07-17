6dea6d5b5d717a4eef31c5be1704e0befca9ad96
ttangzzi <cvbg0802@gmail.com>
2024-07-17 09:30:02 +0900

    useState 변경법

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import { useState } from 'react';
 
 function App() {
   let [title, b] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬 독학']);
+  let [like, likeChg] = useState(0);
   return (
     <div className="App">
       <div className="black-nav">
         <h4>블로그</h4>
       </div>
       <div className="list">
-        <h4>{title[0]}</h4>
+        <span onClick={()=>{b[0]('여자 코트 추천')}}>👚</span>
+        <span>👕</span>
+        <h4>{title[0]} <span onClick={()=>{
+          likeChg(like+1)
+        }}>👍</span> {like} </h4>
         <p>7월 16일 발행</p>
       </div>
       <div className="list">

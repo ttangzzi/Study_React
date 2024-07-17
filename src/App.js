e37806f2b757407d5e969d4f9b41e1e0069ee44a
ttangzzi <cvbg0802@gmail.com>
2024-07-17 11:06:40 +0900

    modal 만들기, map으로 반복문, like 분리

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { useState } from 'react';
 
 function App() {
   let [title, setTitle] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬 독학']);
-  let [like, setLike] = useState(0);
+  let [modal, setModal] = useState(false);
+  let [like, setLike] = useState([0,0,0]);
   return (
     <div className="App">
       <div className="black-nav">
@@ -15,28 +16,37 @@ function App() {
         newTitle.sort();
         setTitle(newTitle);
       }}>정렬</button>
-      <List title={title[0]} date="7월 16일 발행"/>
-      <List title={title[1]} date="7월 16일 발행"/>
-      <List title={title[2]} date="7월 16일 발행"/>
-      <Modal/>
+
+      {
+        title.map((a, i)=>{
+          return (
+            <div className="list" key={i}>
+              <h4 onClick={()=>{
+                modal == true ? setModal(false) : setModal(true);
+              }}>{title[i]}</h4>
+              <span onClick={()=>{
+                let newLike = [...like];
+                newLike[i] += 1;
+                setLike(newLike);
+              }}> 👍</span> {like[i]}
+              <p>7월 17일 작성</p>
+            </div>
+          )
+        })
+      }
+
+      {
+        modal == true ? <Modal title="제목" date="7월 16일 발행"/> : null
+      }
     </div>
   );
 }
 
-function List({title, date}) {
-  return (
-    <div className="list">
-    <h4>{title}</h4>
-    <p>{date}</p>
-  </div>
-  )
-}
-
-function Modal() {
+function Modal({title, date}) {
   return (
     <div className="modal">
-      <h4>제목</h4>
-      <p>날짜</p>
+      <h4>{title}</h4>
+      <p>{date}</p>
       <p>상세내용</p>
     </div>
   )
